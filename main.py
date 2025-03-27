73b3ecd93465c6c11abd82d0997865bc24acf7a3
IliyaBadri <iliya0badri@gmail.com>
2025-03-27 13:27:13 +0330

    Multiple usage with persistant model session.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
 import soundfile as sf
 
+print("[+] Loading model . . .")
+
 # Path to your files
 model_path = "models/coqui_tts/model.pth"
 config_path = "models/coqui_tts/config.json"
@@ -14,11 +16,27 @@ config.load_json(file_name=config_path)
 model = Xtts.init_from_config(config=config)
 model.load_checkpoint(config=config, checkpoint_path=model_path, vocab_path=vocab_path)
 
-output_wav = model.synthesize(
-    config=config,
-    text="What is happening?",
-    speaker_wav= "input.wav", # Provide a speaker wav for cloning, if needed
-    language="en"
-)
+print("[+] Model is loaded.")
 
-sf.write("output.wav", output_wav["wav"], 24000)
+while True:
+    prompt = input("[>] What to say (Default: exit)? ")
+
+    if prompt == "exit" or prompt == "":
+        break
+
+    save_location = input("[>] Where to save (Default: output.wav)? ")
+
+    print("[+] Synthesizing . . .")
+
+    output_wav = model.synthesize(
+        config=config,
+        text=prompt,
+        speaker_wav= "input.wav", # This voice will be cloned
+        language="en"
+    )
+
+    if save_location == "":
+        sf.write("output.wav", output_wav["wav"], 24000)
+    else:
+        sf.write(save_location, output_wav["wav"], 24000)
+    print("[+] Done!")
